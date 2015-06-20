8355ba6433d86795df79f1e6b8892342cd070cc4
Luke Zimmerer <lukezimmerer@users.noreply.github.com>
2015-06-20 23:55:59 +1000

    Removed some magic numbers

--- chip8.hpp
+++ chip8.hpp
@@ -1,14 +1,29 @@
 #ifndef CHIP8_H_INCLUDED
 #define CHIP8_H_INCLUDED
 
-#define CPU_FLAG_DRAW                  0x1
-#define CPU_FLAG_PAUSED                0x2
-#define CPU_FLAG_DETECTCOLLISION       0x8
-#define CPU_FLAG_HWRAP                 0x10
-#define CPU_FLAG_VWRAP                 0x20
-#define CPU_FLAG_DETECTGAMEOVER        0x40
-#define CPU_FLAG_KEYDOWN               0x80
-#define CPU_FLAG_SCHIP                 0x100
+#define CHIP8_MEM_SIZE                  4096
+#define CHIP8_VIDMEM_WIDTH              64
+#define CHIP8_VIDMEM_HEIGHT             32
+#define CHIP8_VIDMEM_SIZE               2048
+#define SCHIP_VIDMEM_WIDTH              128
+#define SCHIP_VIDMEM_HEIGHT             64
+#define SCHIP_VIDMEM_SIZE               8192
+#define CHIP8_STACK_SIZE                16
+#define CHIP8_NUM_REGISTERS             16
+#define SCHIP_NUM_RPLFLAGS              8
+#define CHIP8_NUM_KEYS                  16
+#define CHIP8_FONTSET_SIZE              80
+#define CHIP8_ROM_ADDRESS               0x200
+#define CHIP8_MAX_ROM_SIZE              3584
+
+#define CPU_FLAG_DRAW                   0x1
+#define CPU_FLAG_PAUSED                 0x2
+#define CPU_FLAG_DETECTCOLLISION        0x8
+#define CPU_FLAG_HWRAP                  0x10
+#define CPU_FLAG_VWRAP                  0x20
+#define CPU_FLAG_DETECTGAMEOVER         0x40
+#define CPU_FLAG_KEYDOWN                0x80
+#define CPU_FLAG_SCHIP                  0x100
 
 class Chip8
 {
@@ -16,23 +31,23 @@ class Chip8
     typedef unsigned short  word;
 
     typedef struct {
-        word pc;            // Program counter stores the address of next opcode
-        byte sp;            // Stack pointer points to top of stack
-        word opcode;        // Next opcode to execute, all opcodes are 2 bytes
-        word I;             // Index register used by some instructions
+        word pc;                        // Program counter stores the address of next opcode
+        byte sp;                        // Stack pointer points to top of stack
+        word opcode;                    // Next opcode to execute, all opcodes are 2 bytes
+        word I;                         // Index register used by some instructions
 
-        byte delayTimer;    // Delay timer decrements at 60Hz
-        byte soundTimer;    // Sound timer decrements at 60Hz, beep while non-zero
+        byte delayTimer;                // Delay timer decrements at 60Hz
+        byte soundTimer;                // Sound timer decrements at 60Hz, beep while non-zero
 
-        byte V[16];         // 16 8-bit registers
-        byte R[8];          // 8 8-bit SCHIP flags
+        byte V[CHIP8_NUM_REGISTERS];    // 16 8-bit registers
+        byte R[SCHIP_NUM_RPLFLAGS];     // 8 8-bit SCHIP flags
     } regs;
 
     typedef struct {
         regs _regs;
-        byte _RAM[4096];
-        byte _VRAM[64*32];
-        word _stack[16];
+        byte _RAM[CHIP8_MEM_SIZE];
+        byte _VRAM[CHIP8_VIDMEM_SIZE];
+        word _stack[CHIP8_STACK_SIZE];
     } saveState;
 
     public:
@@ -73,11 +88,10 @@ class Chip8
         //byte _RAM[4096];      // 4096 bytes memory
         //byte _VRAM[128*64];   // 2048 bytes video memory
         //word _stack[16];      // Stack supports 16 subroutine calls
-        byte* _RAM = new byte[4096];
-        byte* _VRAM = new byte[128 * 64];
-        word* _stack = new word[16];
-
-        byte _keyState[16];
+        byte* _RAM = new byte[CHIP8_MEM_SIZE];
+        byte* _VRAM = new byte[SCHIP_VIDMEM_SIZE];
+        word* _stack = new word[CHIP8_STACK_SIZE];
+        byte _keyState[CHIP8_NUM_KEYS];
 };
 
 #endif // CHIP8_H_INCLUDED
