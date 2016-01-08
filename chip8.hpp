ab1852a4201de9d05064e184c8aba0724a10c703
Luke Zimmerer <lukezimmerer@users.noreply.github.com>
2016-01-07 18:58:23 -0800

    Fixed comments and spacing

--- chip8.hpp
+++ chip8.hpp
@@ -1,29 +1,29 @@
 #ifndef CHIP8_H_INCLUDED
 #define CHIP8_H_INCLUDED
 
-#define CHIP8_MEM_SIZE                  4096
-#define CHIP8_VIDMEM_WIDTH              64
-#define CHIP8_VIDMEM_HEIGHT             32
-#define CHIP8_VIDMEM_SIZE               2048
-#define SCHIP_VIDMEM_WIDTH              128
-#define SCHIP_VIDMEM_HEIGHT             64
-#define SCHIP_VIDMEM_SIZE               8192
-#define CHIP8_STACK_SIZE                16
-#define CHIP8_NUM_REGISTERS             16
-#define SCHIP_NUM_RPLFLAGS              8
-#define CHIP8_NUM_KEYS                  16
-#define CHIP8_FONTSET_SIZE              80
-#define CHIP8_ROM_ADDRESS               0x200
-#define CHIP8_MAX_ROM_SIZE              3584
+#define CHIP8_MEM_SIZE           4096
+#define CHIP8_VIDMEM_WIDTH       64
+#define CHIP8_VIDMEM_HEIGHT      32
+#define CHIP8_VIDMEM_SIZE        2048
+#define SCHIP_VIDMEM_WIDTH       128
+#define SCHIP_VIDMEM_HEIGHT      64
+#define SCHIP_VIDMEM_SIZE        8192
+#define CHIP8_STACK_SIZE         16
+#define CHIP8_NUM_REGISTERS      16
+#define SCHIP_NUM_RPLFLAGS       8
+#define CHIP8_NUM_KEYS           16
+#define CHIP8_FONTSET_SIZE       80
+#define CHIP8_ROM_ADDRESS        0x200
+#define CHIP8_MAX_ROM_SIZE       3584
 
-#define CPU_FLAG_DRAW                   0x1
-#define CPU_FLAG_PAUSED                 0x2
-#define CPU_FLAG_DETECTCOLLISION        0x8
-#define CPU_FLAG_HWRAP                  0x10
-#define CPU_FLAG_VWRAP                  0x20
-#define CPU_FLAG_DETECTGAMEOVER         0x40
-#define CPU_FLAG_KEYDOWN                0x80
-#define CPU_FLAG_SCHIP                  0x100
+#define CPU_FLAG_DRAW            0x1
+#define CPU_FLAG_PAUSED          0x2
+#define CPU_FLAG_DETECTCOLLISION 0x8
+#define CPU_FLAG_HWRAP           0x10
+#define CPU_FLAG_VWRAP           0x20
+#define CPU_FLAG_DETECTGAMEOVER  0x40
+#define CPU_FLAG_KEYDOWN         0x80
+#define CPU_FLAG_SCHIP           0x100
 
 class Chip8
 {
@@ -31,16 +31,16 @@ class Chip8
     typedef unsigned short  word;
 
     typedef struct {
-        word pc;                        // Program counter stores the address of next opcode
-        byte sp;                        // Stack pointer points to top of stack
-        word opcode;                    // Next opcode to execute, all opcodes are 2 bytes
-        word I;                         // Index register used by some instructions
+        word pc;                     // Program counter
+        byte sp;                     // Stack pointer
+        word opcode;                 // Next opcode to execute (2 bytes)
+        word I;                      // Index register
 
-        byte delayTimer;                // Delay timer decrements at 60Hz
-        byte soundTimer;                // Sound timer decrements at 60Hz, beep while non-zero
+        byte delayTimer;             // 60Hz delay timer
+        byte soundTimer;             // 60Hz sound timer, beep while non-zero
 
-        byte V[CHIP8_NUM_REGISTERS];    // 16 8-bit registers
-        byte R[SCHIP_NUM_RPLFLAGS];     // 8 8-bit SCHIP flags
+        byte V[CHIP8_NUM_REGISTERS]; // 16 8-bit registers
+        byte R[SCHIP_NUM_RPLFLAGS];  // 8 8-bit SCHIP flags
     } regs;
 
     typedef struct {
@@ -54,31 +54,31 @@ class Chip8
         Chip8();
         ~Chip8();
 
-        byte* GetRAM            ();
-        byte* GetVRAM           ();
-        byte GetDelayTimer      ();
-        byte GetSoundTimer      ();
-        unsigned GetFlags       ();
-        void SetFlags           (const unsigned flags);
-        bool GetFlag            (const unsigned flag);
-        void SetFlag            (const unsigned flag);
-        void ResetFlag          (const unsigned flag);
-        void ToggleFlag         (const unsigned flag);
+        byte* GetRAM       ();
+        byte* GetVRAM      ();
+        byte GetDelayTimer ();
+        byte GetSoundTimer ();
+        unsigned GetFlags  ();
+        void SetFlags      (const unsigned flags);
+        bool GetFlag       (const unsigned flag);
+        void SetFlag       (const unsigned flag);
+        void ResetFlag     (const unsigned flag);
+        void ToggleFlag    (const unsigned flag);
 
-        void Step               ();
-        void DumpRegisters      ();
-        void SetKeys            (const char keyState[16]);
+        void Step          ();
+        void DumpRegisters ();
+        void SetKeys       (const char keyState[16]);
 
-        bool Initialize         ();
-        bool Reset              ();
-        bool LoadRom            (const char* filename);
-        bool UnloadRom          ();
-        void EmulateCycles      (const unsigned nCycles);
-        void ExecuteOpcode      (const word opcode);
-        void SaveState          ();
-        void LoadState          ();
-        void TickDelayTimer     ();
-        void TickSoundTimer     ();
+        bool Initialize    ();
+        bool Reset         ();
+        bool LoadRom       (const char* filename);
+        bool UnloadRom     ();
+        void EmulateCycles (const unsigned nCycles);
+        void ExecuteOpcode (const word opcode);
+        void SaveState     ();
+        void LoadState     ();
+        void TickDelayTimer();
+        void TickSoundTimer();
 
     private:
         regs _regs;
